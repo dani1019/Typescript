94aeb33873b8c98b023e0fe54b1963e4a80ebff5
jooyong yang <nihonkita17@gmail.com>
2024-02-11 10:50:04 +0900

    third commit

--- 3-oop/3-4-abstraction.ts
+++ 3-oop/3-4-abstraction.ts
@@ -6,6 +6,12 @@
     interface CoffeeMaker {
         makeCoffee(shots : number) : CoffeeCup;
     }
+    interface CommercialCoffeeMaker {
+        makeCoffee(shots : number) : CoffeeCup;
+        fillCoffeeBeans(beans: number): void;
+        clean(): void;
+    }
+
     class CoffeeMachine implements CoffeeMaker{
         //shotNumber : number;
         private static BEANS_GRAMM_PER_SHOT : number = 7;
@@ -26,6 +32,7 @@
             this.coffeeBeans += beans;
         }
 
+        clean(){};
         private grindBeans(shots: number){
             console.log(`grinding beans for ${shots}`);
             if(this.coffeeBeans < shots*CoffeeMachine.BEANS_GRAMM_PER_SHOT){
@@ -52,7 +59,39 @@
     }
     const maker: CoffeeMachine = CoffeeMachine.makeMachine(34);
     maker.fillCoffeeBeans(32);
+
     const maker2: CoffeeMaker = CoffeeMachine.makeMachine(34);
-    maker2.fillCoffeeBeans(32);
+    // maker2.fillCoffeeBeans(32); //error
     maker.makeCoffee(2);
+
+    const maker3: CommercialCoffeeMaker = CoffeeMachine.makeMachine(34);
+    // maker2.fillCoffeeBeans(32); //error
+    maker.makeCoffee(2);
+    maker3.clean();
+
+    class AmateurUser{
+        constructor(private machine : CoffeeMaker){
+        }
+        makeCoffee(){
+            const coffee = this.machine.makeCoffee(2);
+            console.log(coffee);
+            }
+    }
+    class ProBarista{
+        constructor(private machine : CommercialCoffeeMaker){
+            }
+        makeCoffee(){
+            const coffee = this.machine.makeCoffee(2);
+            console.log(coffee);
+            this.machine.fillCoffeeBeans(45);
+            this.machine.clean();
+            
+        }
+    }
+    const maker4 : CoffeeMachine = CoffeeMachine.makeMachine(32);
+    const amateur = new AmateurUser(maker);
+    const pro = new ProBarista(maker);
+    amateur.makeCoffee();
+    // amateur.clean(); //error
+    pro.makeCoffee();
 }
